6ff9aaf7febfdf4e59b96f8e15ea91831cf426cb
alizademhdi <alizademhdi@gmail.com>
2023-03-27 18:25:29 +0330

    Add base Game

---
+++ Player.py
@@ -0,0 +1,52 @@
+from Board import BoardUtility
+import random
+
+
+class Player:
+    def __init__(self, player_piece):
+        self.piece = player_piece
+
+    def play(self, board):
+        return 0
+
+
+class RandomPlayer(Player):
+    def play(self, board):
+        return [random.choice(BoardUtility.get_valid_locations(board)), random.choice([1, 2, 3, 4]), random.choice(["skip", "clockwise", "anticlockwise"])]
+
+
+class HumanPlayer(Player):
+    def play(self, board):
+        move = input("row, col, region, rotation\n")
+        move = move.split()
+        return [[int(move[0]), int(move[1])], int(move[2]), move[3]]
+
+
+class MiniMaxPlayer(Player):
+    def __init__(self, player_piece, depth=5):
+        super().__init__(player_piece)
+        self.depth = depth
+
+    def play(self, board):
+        row = -1
+        col = -1
+        region = -1
+        rotation = -1
+        # Todo: implement minimax algorithm
+
+        return [[row, col], region, rotation]
+
+
+class MiniMaxProbPlayer(Player):
+    def __init__(self, player_piece, depth=5, prob_stochastic=0.1):
+        super().__init__(player_piece)
+        self.depth = depth
+        self.prob_stochastic = prob_stochastic
+
+    def play(self, board):
+        row = -1
+        col = -1
+        region = -1
+        rotation = -1
+        # Todo: implement minimaxProb algorithm
+        return [[row, col], region, rotation]
