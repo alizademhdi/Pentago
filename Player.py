74060b6aad48b25c7ac968b42ffbdbfc8e208272
alizademhdi <alizademhdi@gmail.com>
2023-03-29 23:34:27 +0330

    Complete score_position function

--- Player.py
+++ Player.py
@@ -2,6 +2,8 @@ from Board import BoardUtility
 import math
 import random
 
+ROWS = 6
+COLS = 6
 
 class Player:
     def __init__(self, player_piece):
@@ -22,6 +24,150 @@ class HumanPlayer(Player):
         move = move.split()
         return [[int(move[0]), int(move[1])], int(move[2]), move[3]]
 
+class Score:
+    @staticmethod
+    def count_edge(game_board, piece):
+        count = 0
+
+        for c in range(COLS):
+            if game_board[0][c] == piece:
+                count += 1
+            if game_board[ROWS-1][c] == piece:
+                count += 1
+
+        for r in range(ROWS):
+            if game_board[r][0] == piece:
+                count += 1
+            if game_board[r][COLS-1] == piece:
+                count += 1
+
+        return count
+
+    @staticmethod
+    def count_center(game_board, piece):
+        count = 0
+
+        for c in range(2, COLS-2):
+            for r in range(2, ROWS-2):
+                if game_board[r][c] == piece:
+                    count += 1
+
+        return count
+
+    @staticmethod
+    def count_corner(game_board, piece):
+        count = 0
+
+        if game_board[0][0] == piece:
+            count += 1
+        if game_board[0][COLS-1] == piece:
+            count += 1
+        if game_board[ROWS-1][0] == piece:
+            count += 1
+        if game_board[ROWS-1][COLS-1] == piece:
+            count += 1
+
+        return count
+
+    @staticmethod
+    def calculate_value_in_line(line, piece):
+        opposite_piece = 1 if piece == 2 else 2
+
+        empty = line.count(0)
+        own = line.count(piece)
+        opposite = line.count(opposite_piece)
+
+        count_4 = 0
+        for i in range(len(line)-3):
+            if line[i] == line[i+1] == line[i+2] == line[i+3] == piece:
+                count_4 += 1
+
+        if count_4 > 0 and empty >= 1:
+            return 10000
+
+        if own == 4 and empty >= 1:
+            return 1000
+
+        if opposite == 4 and empty >= 1:
+            return -5000
+
+        value = 0
+        count_3 = 0
+        for i in range(len(line)-2):
+            if line[i] == line[i+1] == line[i+2] == piece:
+                count_3 += 1
+        if count_3 > 0 and empty >= 2:
+            value += 50
+
+        if opposite >=3:
+            value -= 30
+
+        count_2 = 0
+        for i in range(len(line)-1):
+            if line[i] == line[i+1] == piece:
+                count_2 += 1
+        if count_2 > 0 and empty >= 3:
+            value += 30
+
+        return value
+
+    @staticmethod
+    def calculate_value(game_board, piece):
+        value = 0
+        for r in range(ROWS):
+            row = [i for i in list(game_board[r,:])]
+            for c in range(COLS-4):
+                line = row[c:c+5]
+                value += Score.calculate_value_in_line(line, piece)
+
+        for c in range(COLS):
+            column = [i for i in list(game_board[:,c])]
+            for r in range(ROWS-4):
+                line = column[r:r+5]
+                value += Score.calculate_value_in_line(line, piece)
+
+        for c in range(COLS-4):
+            for r in range(ROWS-4):
+                line = [game_board[r+i][c+i] for i in range(5)]
+                value += Score.calculate_value_in_line(line, piece)
+
+        for c in range(COLS-4):
+            for r in range(ROWS-4):
+                line = [game_board[r+3-i][c+i] for i in range(5)]
+                value += Score.calculate_value_in_line(line, piece)
+
+        value += Score.count_edge(game_board, piece) * 10 + Score.count_center(game_board, piece) * 5 + Score.count_corner(game_board, piece) * 20
+
+        return value
+
+
+
+    @staticmethod
+    def score_position(game_board, piece):
+        """
+        compute the game board score for a given piece.
+        you can change this function to use a better heuristic for improvement.
+        """
+        value = 0
+        if BoardUtility.has_player_won(game_board, piece):
+            return 100_000_000_000  # player has won the game give very large score
+        if BoardUtility.has_player_won(game_board, 1 if piece == 2 else 2):
+            return -100_000_000_000  # player has lost the game give very large negative score
+
+
+        value += Score.calculate_value(game_board, piece)
+
+        for region in BoardUtility.REGIONS:
+            for rotate in ["clockwise", "anticlockwise"]:
+                b = game_board.copy()
+                BoardUtility.rotate_region(b, region, rotate)
+                value += Score.calculate_value(b, piece)
+                if BoardUtility.has_player_won(b, piece):
+                    return 100_000_000_000
+                if BoardUtility.has_player_won(b, 1 if piece == 2 else 2):
+                    return -100_000_000_000
+
+        return value
 
 class MiniMaxPlayer(Player):
     def __init__(self, player_piece, depth=5):
@@ -31,7 +177,7 @@ class MiniMaxPlayer(Player):
     @staticmethod
     def minimax(board, depth, piece, alpha, beta, is_max):
         if BoardUtility.is_terminal_state(board) or depth == 0:
-            return BoardUtility.score_position(board, piece), None
+            return Score.score_position(board, piece), None
 
         value = -100_000_000_000 if is_max == 1 else 100_000_000_000
         locations = BoardUtility.get_valid_locations(board)
@@ -74,7 +220,7 @@ class MiniMaxProbPlayer(Player):
     @staticmethod
     def minimax_prob(board, depth, piece, alpha, beta, is_max, prob):
         if BoardUtility.is_terminal_state(board) or depth == 0:
-            return BoardUtility.score_position(board, piece), None
+            return Score.score_position(board, piece), None
 
         value = -100_000_000_000 if is_max == 1 else 100_000_000_000
         locations = BoardUtility.get_valid_locations(board)
